bca3b2acc166204175ba8108f2492641d8eff7a4
simonetome <10622900@polimi.it>
2021-12-17 18:18:26 +0100

    Class balancement.

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import os 
 from scipy.io import loadmat 
+import matplotlib.pyplot as plt 
 
 def load_data(sample_prefix, input_dir):    # everything is returned as a numpy array which is easier to manipulate
   label_filepath = os.path.join(input_dir, sample_prefix + '_ann.mat')
@@ -11,9 +12,33 @@ def load_data(sample_prefix, input_dir):    # everything is returned as a numpy 
     label = np.asarray(mat_file['labels'])
   if os.path.isfile(peak_filepath):
     mat_file = loadmat(peak_filepath)
-    peak = np.asarray(mat_file['rpeaks'])
+    peaks = np.array(mat_file['rpeaks'])
+    peak = list()
+    for p in peaks:
+      peak.append(p[0])
+
   if os.path.isfile(signal_filepath):
     mat_file = loadmat(signal_filepath)
     signal = np.asarray(mat_file['ecg'] )
 
   return label, peak, signal
+
+# this function creates a patch for the defined signal & peak and save it in the "dataset_folder/Patches" folder 
+# under the name of [SIGNAL_ID]_[peakID]_[LABEL].png
+
+# there is a problem: sopraventricular beats (PAC complex) are hard to distinguish from the normal sinus beat
+def create_patch(ecg_signals,rpeaks,sigID,peakID):
+    
+    signal = ecg_signals[sigID]
+
+    prev = rpeaks[sigID][peakID-1]
+    peak = rpeaks[sigID][peakID]
+    succ = rpeaks[sigID][peakID+1] 
+    x = np.arange(int((peak+prev)/2),int((peak+succ)/2)) # lowest x is equal to the average between the peakID and the previous, dual for the highest x 
+    y = signal[x,0]
+    fig, ax = plt.subplots()
+    ax.plot(x, y, linewidth=2.0)
+    
+    plt.axis('off')         # axes are useless in input to the model 
+    #plt.savefig(dataset_folder+"//Patches//"+str(ids[sigID])+"_"+str(peakID)+"_"+labels[sigID][peakID]+".png",format="png",facecolor = "white",bbox_inches='tight',pad_inches = 0) # save the image 
+   
